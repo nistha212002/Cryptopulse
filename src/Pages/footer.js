f4dd9db808d6078cfb2b097b9f2c1bda377fa486
nistha212002 <nisthasinghindore@gmail.com>
2024-06-20 11:35:02 +0530

    first commit

---
+++ src/Pages/footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div style={{
+            paddingTop: 5,
+            paddingDown: 30,
+            paddingRight: 30,
+            textAlign: "right"
+        }}>
+            Developed by Nistha ©
+
+        </div>
+    );
+}
+
+export default Footer
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHlgH0j4E0KoKe4UGG8lFijSK_m6Qs13Q",
+  authDomain: "crypto-pulse-3o.firebaseapp.com",
+  projectId: "crypto-pulse-3o",
+  storageBucket: "crypto-pulse-3o.appspot.com",
+  messagingSenderId: "268762423179",
+  appId: "1:268762423179:web:6604ff08f132e2b65d598e"
+};
+
+export default firebaseConfig; 
